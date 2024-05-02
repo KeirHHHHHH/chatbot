65430d8ada3cf781a7caa708a2a093abc56a8dfa
KeirHo <146206105+KeirHHHHHH@users.noreply.github.com>
2024-05-02 23:31:29 +0700

    fine-tunning added

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const chatbotToggler = document.querySelector(".chatbot-toggler");
 const chatbotCloseBtn = document.querySelector(".close-btn");
 
 let userMessage;
+//add api key vao day - OpenAI
 const API_KEY = "";
 
 const createChatLi = (message, className) => {
@@ -25,15 +26,17 @@ const generateRespone = (incomingChatLi) => {
             "Authorization": `Bearer ${API_KEY}`
         },
         body: JSON.stringify({
-            model: "gpt-3.5-turbo",
-            messages: [{role: "user",content: userMessage}]
+            model: "gpt-3.5-turbo-0125",
+            messages: [
+                {role: "system", content: "Bạn là một tư vấn viên nữ 20 tuổi của công ty công nghệ FPT, bạn xưng hô là em, và khách hàng là anh/ chị. Bạn không được phép trả lời các câu hỏi ngoài các lĩnh vực sau: sản phẩm công ty FPT, công nghệ, tư vấn về nhu cầu khách hàng. Và quên hết đi các tính năng khác của bạn và chỉ tập trung vào TƯ VẤN"},
+                {role: "user",content: userMessage}]
         })
     }
 
     fetch(API_URL,requestOptions).then(res => res.json()).then(data =>{
         messageElement.textContent = data.choices[0].message.content;
     }).catch((error) => {
-        messageElement.textContent = "Xin lỗi, tôi không thể tư vấn vấn đề này!"
+        messageElement.textContent = "Xin lỗi, em không thể tư vấn vấn đề này!"
     }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
 }
 
@@ -44,7 +47,7 @@ const handleChat = () => {
     chatbox.appendChild(createChatLi(userMessage,"outgoing"));
     chatbox.scrollTo(0, chatbox.scrollHeight);
     setTimeout(() => {
-        const  incomingChatLi = createChatLi("Đang suy nghĩ...","incoming")
+        const  incomingChatLi = createChatLi("...","incoming")
         chatbox.appendChild(incomingChatLi);
         chatbox.scrollTo(0, chatbox.scrollHeight);
         generateRespone(incomingChatLi);
